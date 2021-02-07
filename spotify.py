104032d60620ba2054bb33bd01ceb43dd36486df
Maxwell Warsaw <Max585t@gmail.com>
2021-02-06 20:41:39 -0700

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -1,43 +1,51 @@
-import argparse
-import logging
-
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-#import requests
-#import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import json
 
-secret_codes = json.load(open('secrets.json', 'r'))
+def set_up():
+    secret_codes = json.load(open('secrets.json', 'r'))
 
-spotify_client_id = secret_codes.get('spotify_client_id')
-spotify_client_secret = secret_codes.get('spotify_client_secret')
+    spotify_client_id = secret_codes.get('spotify_client_id')
+    spotify_client_secret = secret_codes.get('spotify_client_secret')
 
-client_credential_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-#sp = spotipy.Spotify(client_credentials_manager=client_credential_manager)
-
-logger = logging.getLogger('examples.create_playlist')
-logging.basicConfig(level='DEBUG')
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Creates a playlist for user')
-    parser.add_argument('-p', '--playlist', required=True,
-                        help='Name of Playlist')
-    parser.add_argument('-d', '--description', required=False, default='',
-                        help='Description of Playlist')
-    return parser.parse_args()
-
-
-def main():
-    args = get_args()
-    scope = "playlist-modify-public"
+    client_credential_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credential_manager)
-    user_id = sp.me()['id']
-    print(user_id)
-    # sp.user_playlist_create(user_id, args.playlist)
+    return sp
 
+def get_id_from_album_name(sp, album):
+    results = sp.search(q='album:' + album, type='album')
+    return results
 
-if __name__ == '__main__':
-    main()
+def get_id_from_song_name(sp, song):
+    results = sp.search(q='song:' + song, type='song')
+    return results
+
+def get_id_from_artist_name(sp, artist):
+    results = sp.search(q='artist:' + artist, type='artist')
+    return results
+
+def check_id_is_correct(sp, id, name, id_type):
+    # get the results from spottify
+    if 'song' in id_type:
+        results = sp.song(id)
+    if 'album' in id_type:
+        results = sp.album(id)
+    if 'artist' in id_type:
+        results = sp.artist(id)
+    
+    # check if results is the same as name
+    if name in results:
+        return True
+    return False
+
+def create_playlist(sp, playlist_name='Discogs Collection'):
+    user_name = sp.current_user()
+    playlist = sp.create_playlist(user_name, name=playlist_name)
+    return playlist
+
+def update_playlist(sp, playlist_id, ids_to_add):
+    user_name = sp.currect_urser()
+    for song in ids_to_add:
+        sp.user_playist_add_tracts(user_name, playlist_id, song)
+    return True
